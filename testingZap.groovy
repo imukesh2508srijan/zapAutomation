bcf7382702178944bac5c8fd7782bf5612525a0e
Mukesh Sharma <mukesh.sharma@mukeshs-MacBook-Pro.local>
2020-09-09 10:42:11 +0530

    Formatted Shell Variable

--- testingZap.groovy
+++ testingZap.groovy
@@ -9,35 +9,35 @@ pipeline {
     stage('Open URL'){
         steps {
             sh "TARGET_URL='https://natixis-stage.oncorps.io'"
-            sh "docker exec $CONTAINER_ID zap-cli -p 2375 status -t 120 && docker exec $CONTAINER_ID zap-cli -p 2375 open-url $TARGET_URL"
+            sh "docker exec ${CONTAINER_ID} zap-cli -p 2375 status -t 120 && docker exec ${CONTAINER_ID} zap-cli -p 2375 open-url ${TARGET_URL}"
         }
     }
     stage('Execute Spider Scan'){
         steps {
-            sh "docker exec $CONTAINER_ID zap-cli -p 2375 spider $TARGET_URL"
+            sh "docker exec ${CONTAINER_ID} zap-cli -p 2375 spider ${TARGET_URL}"
         }
     }
     stage('Execute Active Scan'){
         steps {
-            sh "docker exec $CONTAINER_ID zap-cli -p 2375 active-scan -r $TARGET_URL"
+            sh "docker exec ${CONTAINER_ID} zap-cli -p 2375 active-scan -r ${TARGET_URL}"
         }
     }
     stage('Log Alerts'){
         steps {
-            sh "docker exec $CONTAINER_ID zap-cli -p 2375 alerts"
+            sh "docker exec ${CONTAINER_ID} zap-cli -p 2375 alerts"
         }
     }
     stage('Generate HTML Report'){
         steps {
-            sh "docker exec $CONTAINER_ID zap-cli -p 2375 report -o ZAP_Report.html -f html"
-            sh "sudo docker cp $CONTAINER_ID:/zap/ZAP_Report.html /home/mukesh/zapTempDir/reports/ZAP_Report.html"
+            sh "docker exec ${CONTAINER_ID} zap-cli -p 2375 report -o ZAP_Report.html -f html"
+            sh "sudo docker cp ${CONTAINER_ID}:/zap/ZAP_Report.html /home/mukesh/zapTempDir/reports/ZAP_Report.html"
         }
     }
     stage('Stop and Remove Container'){
         steps {
-            sh "docker logs $CONTAINER_ID"
-            sh "docker stop $CONTAINER_ID"
-            sh "docker rm $CONTAINER_ID"
+            sh "docker logs ${CONTAINER_ID}"
+            sh "docker stop ${CONTAINER_ID}"
+            sh "docker rm ${CONTAINER_ID}"
         }
     }       
     }
