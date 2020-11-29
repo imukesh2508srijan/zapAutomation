7cf126128af6fe9e91c3a99689f08b0260723c03
Mukesh Sharma <mukesh.sharma@mukeshs-MacBook-Pro.local>
2020-11-29 11:40:14 +0530

    Testing

--- scanneredited.groovy
+++ scanneredited.groovy
@@ -1,17 +1,47 @@
 pipeline {
     agent any
     stages {
-        stage('Example') {
-            steps {
-                echo 'Hello World'
 
+        stage('Stage 1') {
+            steps {
+                echo 'Stage 1'
                 script {
-                    def browsers = ['chrome', 'firefox']
-                    for (int i = 0; i < browsers.size(); ++i) {
-                        echo "Testing the ${browsers[i]} browser"
                     }
                 }
             }
+
+        stage('Stage 2') {
+            steps {
+                echo 'Stage 2'
+                script {
+                    }
+                }
+            }
+
+        stage('Stage 3') {
+            steps {
+                echo 'Stage 3'
+                script {
+                    }
+                }
+            }
+
+        stage('Stage 4') {
+            steps {
+                echo 'Stage 4'
+                script {
+                    }
+                }
+            }
+
+        stage('Stage 5') {
+            steps {
+                echo 'Stage 5'
+                script {
+                    }
+                }
+            }
+
         }
     }
 }
